1ea1b4b956c567b17a668e061b45529a85c84281
Prescott Murphy <prurph@gmail.com>
2014-02-21 17:31:06 -0500

    Add uid to generate unique artist and album ids

---
+++ src/RCApp.js
@@ -0,0 +1,13 @@
+var RCApp = {
+  uid: (function(){
+    var uidArtist = 0;
+    var uidAlbum  = 0;
+    return function(type){
+      return (type === "artist") ? uidArtist++ : uidAlbum++;
+    };
+  })(),
+  create:
+  deleteThing: function(collection, targetid) {
+
+  }
+};
